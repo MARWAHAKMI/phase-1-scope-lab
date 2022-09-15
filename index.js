9c61d1e5b75de53f37493bb4c3fc86a093de20f2
Marwa <marwahakmi@gmail.com>
2022-09-15 10:29:59 +0300

    assignement completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    this.customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+function setBestCustomer(str) {
+    this.bestCustomer = "not bob";
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    this.bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = "bob";
+    leastFavoriteCustomer = "not bob";
+}
